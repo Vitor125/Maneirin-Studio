9b87733eb826ebfff1ae5ab47aa85eeee6f086d6
Vitor <vitortrindadeborher>
2026-05-21 19:30:38 -0300

    Fix API_URL for production deployment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = window.MANEIRIN_API_URL || 'http://127.0.0.1:8000/api';
+const API_URL = window.MANEIRIN_API_URL || '/api';
 const WHATSAPP_PHONE = '5521980453636';
 
 function escapeHtml(value = '') {
